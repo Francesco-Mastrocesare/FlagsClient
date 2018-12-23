312a1adbe9fcf7802e8e5cf37818b91292e79ff0
Francesco-Mastrocesare <francesco.mastrocesare@gmail.com>
2018-12-23 14:57:21 +0100

    deploy

--- js/index.js
+++ js/index.js
@@ -5,35 +5,30 @@ $(document).ready(
         $("#studentPost").on('submit',function(e){
             e.preventDefault();
             var result = { };
-           $.each($(this).serializeArray(), function() {
-               result[this.name] = this.value;
-           });
-           result = JSON.stringify(result,null,' ');
-           
-           
-        //    $.get("https://restcountries.eu/rest/v2/name/"+ countryUrl +"?fullText=true",function(data){
-                
-                
-        //     });
-        //    console.log(result);
-
-           $.ajax({
-               type: 'POST',
-               url: 'https://localhost:44369/api/Flags',
-               data: result,
-               contentType: "application/json",
-               dataType: 'json',
-               statusCode: {
-                   200: function(data) {
-                       $("#imageFlag").attr("src",data.responseText);
-                       //document["#imageFlag"].src = res;
-                        //alert (data.responseText);
-                   },
-                   400: function() {
-                       alert( "Bad Request 404" );
-                   }
-               }
-           });
+            $.each($(this).serializeArray(), function() {
+                result[this.name] = this.value;
+            });
+            var req = {};
+            var country = "https://restcountries.eu/rest/v2/name/"+ result.nome +"?fullText=true"
+            $.get(country ,function(data){
+                    req['nome']=data[0].alpha2Code;
+                    var sender = JSON.stringify(req);
+                    $.ajax({
+                        type: 'POST',
+                        url: 'https://flagsapi.azurewebsites.net/api/Flags',
+                        data: sender,
+                        contentType: "application/json",
+                        dataType: 'json',
+                        statusCode: {
+                            200: function(data2) {
+                                $("#imageFlag").attr("src",data2.responseText);
+                            },
+                            400: function() {
+                                alert( "Bad Request 404" );
+                            }
+                        }
+                    });
+                });
             });
        
 
@@ -42,26 +37,7 @@ $(document).ready(
         
     });
 
-    function createRow(student){
-        return "<tr class='rigaTabella'>"+createColumn(student.id)+createColumn(student.nomeCompleto)+
-                createColumn(student.corsi.length)+"</tr>";
-    }
-
-    function createColumn(data){
-        return "<td>"+data+"</td>";
-    }
 
     function loadData(){
-        $(".rigaTabella").html("");
-        
-        $.get("https://modisapi20181211015957.azurewebsites.net/api/Studenti",function(data){
-            for(var i=0;i<data.length; i++){
-                //alert(data[i].nomeCompleto);
-                $("#studentTable").append(
-                    createRow(data[i])
-                );
-
-            }
-                
-        });
+      
     }
